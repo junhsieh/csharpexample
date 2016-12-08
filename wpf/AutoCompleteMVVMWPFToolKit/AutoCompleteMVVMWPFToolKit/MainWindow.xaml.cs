99c0f4e7f5cfd0864eff0cacd33d54fa01c416d9
Jun Hsieh <jun.github@ijun.org>
2016-12-08 15:16:20 -0800

    Update

--- wpf/AutoCompleteMVVMWPFToolKit/AutoCompleteMVVMWPFToolKit/MainWindow.xaml.cs
+++ wpf/AutoCompleteMVVMWPFToolKit/AutoCompleteMVVMWPFToolKit/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using AutoCompleteMVVMWPFToolKit.Module.SrvReq;
+using System.Windows;
 
 namespace AutoCompleteMVVMWPFToolKit
 {
@@ -15,8 +16,32 @@ namespace AutoCompleteMVVMWPFToolKit
         private void SrvReqBtn_Click(object sender, RoutedEventArgs e)
         {
             Module.SrvReq.MainWindow w = new Module.SrvReq.MainWindow();
-
             w.Owner = Window.GetWindow(this);
+
+            w.MainWindowViewModel.SrvReqCore.ItemSingle.OptArr.Add(new Item() { ID = 1, ItemNum = "CXR51A", ItemName = "Computer Regular Black" });
+            w.MainWindowViewModel.SrvReqCore.ItemSingle.OptArr.Add(new Item() { ID = 2, ItemNum = "CXR52A", ItemName = "Computer Regular Red" });
+            w.MainWindowViewModel.SrvReqCore.ItemSingle.OptArr.Add(new Item() { ID = 3, ItemNum = "CXR53A", ItemName = "Computer Regular Green" });
+
+            w.MainWindowViewModel.SrvReqCore.WarrantyMultiple.OptArr.Add(new Warranty() { ID = 1, Text = "Send parts" });
+            w.MainWindowViewModel.SrvReqCore.WarrantyMultiple.OptArr.Add(new Warranty() { ID = 2, Text = "Return for repair" });
+            w.MainWindowViewModel.SrvReqCore.WarrantyMultiple.OptArr.Add(new Warranty() { ID = 3, Text = "Return for credit" });
+            w.MainWindowViewModel.SrvReqCore.WarrantyMultiple.OptArr.Add(new Warranty() { ID = 4, Text = "Create Invoice" });
+
+            w.MainWindowViewModel.SrvReqCore.CountrySingle.OptArr.Add(new Country() { ID = 1, Text = "Canada" });
+            w.MainWindowViewModel.SrvReqCore.CountrySingle.OptArr.Add(new Country() { ID = 2, Text = "USA" });
+            w.MainWindowViewModel.SrvReqCore.CountrySingle.OptArr.Add(new Country() { ID = 3, Text = "China" });
+            w.MainWindowViewModel.SrvReqCore.CountrySingle.OptArr.Add(new Country() { ID = 4, Text = "Japan" });
+
+            w.MainWindowViewModel.SrvReqCore.DealerSingle.OptArr.Add(new Dealer() { ID = 1, DealerName = "aaa 1", City = "city 1" });
+            w.MainWindowViewModel.SrvReqCore.DealerSingle.OptArr.Add(new Dealer() { ID = 2, DealerName = "aaa 2", City = "city 2" });
+            w.MainWindowViewModel.SrvReqCore.DealerSingle.OptArr.Add(new Dealer() { ID = 3, DealerName = "aaa 3", City = "city 3" });
+            w.MainWindowViewModel.SrvReqCore.DealerSingle.OptArr.Add(new Dealer() { ID = 4, DealerName = "bbb 4", City = "city 4" });
+            w.MainWindowViewModel.SrvReqCore.DealerSingle.OptArr.Add(new Dealer() { ID = 5, DealerName = "bbb 5", City = "city 5" });
+            w.MainWindowViewModel.SrvReqCore.DealerSingle.OptArr.Add(new Dealer() { ID = 6, DealerName = "bbb 6", City = "city 6" });
+            w.MainWindowViewModel.SrvReqCore.DealerSingle.OptArr.Add(new Dealer() { ID = 7, DealerName = "ccc 7", City = "city 7" });
+            w.MainWindowViewModel.SrvReqCore.DealerSingle.OptArr.Add(new Dealer() { ID = 8, DealerName = "ccc 8", City = "city 8" });
+            w.MainWindowViewModel.SrvReqCore.DealerSingle.OptArr.Add(new Dealer() { ID = 9, DealerName = "ccc 9", City = "city 9" });
+
             w.ShowDialog();
         }
     }
--- wpf/AutoCompleteMVVMWPFToolKit/AutoCompleteMVVMWPFToolKit/Module/SrvReq/MainWindowViewModel.cs
+++ wpf/AutoCompleteMVVMWPFToolKit/AutoCompleteMVVMWPFToolKit/Module/SrvReq/MainWindowViewModel.cs
@@ -96,41 +96,10 @@ namespace AutoCompleteMVVMWPFToolKit.Module.SrvReq
 
         public SrvReqCore()
         {
-            //
             this.ItemSingle = new ViewModel.ObjectOptSingle<Item>();
-            this.ItemSingle.OptArr.Add(new Item() { ID = 1, ItemNum = "CXR51A", ItemName = "Computer Regular Black" });
-            this.ItemSingle.OptArr.Add(new Item() { ID = 2, ItemNum = "CXR52A", ItemName = "Computer Regular Red" });
-            this.ItemSingle.OptArr.Add(new Item() { ID = 3, ItemNum = "CXR53A", ItemName = "Computer Regular Green" });
-
-            this.IDItem = 3;
-
-            //
             this.WarrantyMultiple = new ViewModel.ObjectOptMultiple<Warranty>();
-            this.WarrantyMultiple.OptArr.Add(new Warranty() { ID = 1, Text = "Send parts" });
-            this.WarrantyMultiple.OptArr.Add(new Warranty() { ID = 2, Text = "Return for repair" });
-            this.WarrantyMultiple.OptArr.Add(new Warranty() { ID = 3, Text = "Return for credit" });
-            this.WarrantyMultiple.OptArr.Add(new Warranty() { ID = 4, Text = "Create Invoice" });
-
-            //
             this.CountrySingle = new ViewModel.ObjectOptSingle<Country>();
-            this.CountrySingle.OptArr.Add(new Country() { ID = 1, Text = "Canada" });
-            this.CountrySingle.OptArr.Add(new Country() { ID = 2, Text = "USA" });
-            this.CountrySingle.OptArr.Add(new Country() { ID = 3, Text = "China" });
-            this.CountrySingle.OptArr.Add(new Country() { ID = 4, Text = "Japan" });
-
-            //
             this.DealerSingle = new ViewModel.ObjectOptSingle<Dealer>();
-            this.DealerSingle.OptArr.Add(new Dealer() { ID = 1, DealerName = "aaa 1", City = "city 1" });
-            this.DealerSingle.OptArr.Add(new Dealer() { ID = 2, DealerName = "aaa 2", City = "city 2" });
-            this.DealerSingle.OptArr.Add(new Dealer() { ID = 3, DealerName = "aaa 3", City = "city 3" });
-            this.DealerSingle.OptArr.Add(new Dealer() { ID = 4, DealerName = "bbb 4", City = "city 4" });
-            this.DealerSingle.OptArr.Add(new Dealer() { ID = 5, DealerName = "bbb 5", City = "city 5" });
-            this.DealerSingle.OptArr.Add(new Dealer() { ID = 6, DealerName = "bbb 6", City = "city 6" });
-            this.DealerSingle.OptArr.Add(new Dealer() { ID = 7, DealerName = "ccc 7", City = "city 7" });
-            this.DealerSingle.OptArr.Add(new Dealer() { ID = 8, DealerName = "ccc 8", City = "city 8" });
-            this.DealerSingle.OptArr.Add(new Dealer() { ID = 9, DealerName = "ccc 9", City = "city 9" });
-
-            this.IDDealer = 3;
         }
     }
 
