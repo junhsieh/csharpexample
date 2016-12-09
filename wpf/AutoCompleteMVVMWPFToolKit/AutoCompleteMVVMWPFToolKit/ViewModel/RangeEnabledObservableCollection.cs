69bbca022cffe0e7eb858eb1c6048d38b201bd2f
Jun Hsieh <jun.github@ijun.org>
2016-12-09 05:05:20 -0800

    Update

--- wpf/AutoCompleteMVVMWPFToolKit/AutoCompleteMVVMWPFToolKit/ViewModel/RangeEnabledObservableCollection.cs
+++ wpf/AutoCompleteMVVMWPFToolKit/AutoCompleteMVVMWPFToolKit/ViewModel/RangeEnabledObservableCollection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,22 @@ namespace AutoCompleteMVVMWPFToolKit.ViewModel
         public void AddRange(IEnumerable<T> items)
         {
             this.CheckReentrancy();
+
             foreach (var item in items)
             {
                 this.Items.Add(item);
             }
+
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+            this.OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
             this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public void Reset(IEnumerable<T> items)
+        {
+            this.Items.Clear();
+
+            AddRange(items);
         }
     }
 }
