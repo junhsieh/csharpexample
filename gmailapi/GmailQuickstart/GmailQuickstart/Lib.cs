8b96beca33d2d687f7081f71697e3aaeefb34fc5
Jun Hsieh <jun.github@ijun.org>
2017-01-11 11:41:33 -0800

    Update

--- gmailapi/GmailQuickstart/GmailQuickstart/Lib.cs
+++ gmailapi/GmailQuickstart/GmailQuickstart/Lib.cs
@@ -8,6 +8,9 @@ namespace GmailQuickstart
 {
     static public class Lib
     {
+        // Reference:
+        // http://stackoverflow.com/questions/1228701/code-for-decoding-encoding-a-modified-base64-url
+        // https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-08#appendix-C
         static public string base64urlencode(byte[] arg)
         {
             string s = Convert.ToBase64String(arg); // Regular base64 encoder
@@ -28,8 +31,7 @@ namespace GmailQuickstart
                 case 2: s += "=="; break; // Two pad chars
                 case 3: s += "="; break; // One pad char
                 default:
-                    throw new System.Exception(
-             "Illegal base64url string!");
+                    throw new System.Exception("Illegal base64url string!");
             }
             return Convert.FromBase64String(s); // Standard base64 decoder
         }
